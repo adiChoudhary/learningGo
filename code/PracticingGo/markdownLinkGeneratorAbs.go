61f854ebc88def4d0be6d2d6ca52cae8ee26bfe8
Aditya Choudhary <adichoudhary5739@gmail.com>
2024-01-12 08:39:47 +0530

    feat: added map logic to establish directory hirarchy

--- code/PracticingGo/markdownLinkGeneratorAbs.go
+++ code/PracticingGo/markdownLinkGeneratorAbs.go
@@ -15,14 +15,35 @@ func main() {
 		return
 	}
 
+	mp := make(map[string]interface{})
+
 	for _, filePath := range filePaths {
 		relPath, _ := filepath.Abs(filePath)
 		relPath = strings.Replace(relPath, "/home/adi/Documents/coding/learningGo/code/PracticingGo/", "", 1)
 		temp := strings.Split(relPath, "/")
-		linkText := temp[len(temp)-1]
-		link := fmt.Sprintf("[%s](https://github.com/adiChoudhary/learningGo/blob/main/code/PracticingGo/%s)\n", linkText, relPath)
-		fmt.Println(link)
+		//linkText := temp[len(temp)-1]
+		currentMp := mp
+		for index, value := range temp {
+			if strings.Contains(value, ".go") {
+				continue
+			}
+			if _, ok := currentMp[value]; (!ok) && (len(temp)-2) != index {
+				currentMp[value] = make(map[string]interface{})
+			} else if (!ok) && (len(temp)-2) == index {
+				currentMp[value] = []string{temp[len(temp)-1]}
+			} else if (ok) && (len(temp)-2) == index {
+				currentMp[value] = append(currentMp[value].([]string), temp[len(temp)-1])
+			}
+			// Here type assertion is used to tell go a specific type since
+			// interface isn't a type
+			if (len(temp) - 2) != index {
+				currentMp = currentMp[value].(map[string]interface{})
+			}
+		}
+		//mp[temp[0]] = "Hello"
+		//link := fmt.Sprintf("[%s](https://github.com/adiChoudhary/learningGo/blob/main/code/PracticingGo/%s)\n", linkText, relPath)
 	}
+	fmt.Println(mp)
 }
 
 func getAllGoFiles(rootDir string) ([]string, error) {
